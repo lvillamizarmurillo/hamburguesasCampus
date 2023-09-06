964d975e859f4372b19b3ad0643833dbdd9b2ee8
lvillamizarmurillo <lvillamizarmurillo@gmail.com>
2023-09-06 18:59:21 -0500

    feat: :sparkles: Punto 9
    Encontrar todas las hamburguesas que contienen "Pan integral" como ingrediente

--- src/routes/v1/hamburguesas.js
+++ src/routes/v1/hamburguesas.js
@@ -8,6 +8,8 @@ const version = routesVersioning();
 
 router.get('/chef', version({ "1.0.0": Chefs.getChefCarnes,"1.0.1": Chefs.getBurguerChef}));
 
+router.get('/', version({ "1.0.0": Chefs.getPanIntegral}));
+
 router.post('/', version({ "1.0.0": Chefs.postBurguer}));
 
 export {
